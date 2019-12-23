ea737298a01807d04ffef4a78d773fd70269da96
resilientbd <faisal.hossain.pk@gmail.com>
2019-12-24 05:06:33 +0600

    init

---
+++ app/src/main/java/com/shadowhite/ujala/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shadowhite.ujala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
